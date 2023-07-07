d8e55440a9618dcee56aeabe045ed04360ad2b51
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-07-07 16:57:59 -0300

    Auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    return NextResponse.json("Not auth", { status: 400 });
+  }
+}
+
+export const config = {
+  matcher: "/api/products/:path*",
+};
