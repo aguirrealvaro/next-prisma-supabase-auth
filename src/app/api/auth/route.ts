a4e0cdc2273cdc1a060ac965ace71b2e3c96c69c
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-07-07 01:38:07 -0300

    Create endpoint to login

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,15 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const supabase = createRouteHandlerClient({ cookies });
+  const { email, password } = await request.json();
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  return NextResponse.json({ data, error });
+}
