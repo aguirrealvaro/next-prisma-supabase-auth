f196bf91b96f9b9250b348fdf11795a8834edbe6
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-07-07 15:41:06 -0300

    Check existance of user on register

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,10 +1,17 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+import prisma from "@/utils/prisma";
 
 export async function POST(request: Request) {
   const supabase = createRouteHandlerClient({ cookies });
   const { email, password } = await request.json();
+
+  const profile = await prisma.profile.findFirst({ where: { email } });
+
+  if (profile) {
+    return NextResponse.json("email alredy exists", { status: 400 });
+  }
 
   const { data, error } = await supabase.auth.signUp({
     email,
