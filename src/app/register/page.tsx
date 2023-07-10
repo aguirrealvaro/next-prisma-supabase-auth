d3177aa931e6e3b91cfed27fb923536f3dc52048
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-07-10 12:49:43 -0300

    init form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,34 @@
-import { FunctionComponent } from "react";
+"use client";
+
+import { FormEvent, FunctionComponent } from "react";
 
 export const Register: FunctionComponent = () => {
-  return <div>Register</div>;
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="mb-4 last:mb-4">
+          <label htmlFor="email">Email: </label>
+          <input type="text" id="email" className="rounded border border-neutral-600 px-2" />
+        </div>
+        <div className="mb-4 last:mb-4">
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            className="rounded border border-neutral-600 px-2"
+          />
+        </div>
+        <button type="submit" className="rounded bg-blue-600 p-2 text-white">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
